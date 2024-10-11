000ac368530806cbfa924fe9b47d35a6f6453394
bishlrajbanshi <bishalrajbanshi.in@gmail.com>
2024-10-11 20:36:00 +0545

    12

--- guss number/script.js
+++ guss number/script.js
@@ -2,3 +2,11 @@ let gussNum = document.getElementById('generatenum')
 let userNum = document.getElementById('usernum')
 let displayNum = document.getElementById('display')
 
+let randNum = Math.floor(Math.random() * 100) + 1
+
+
+gussNum.addEventListener('click',(e)=>{
+    if (userNum.value == randNum) {
+        
+    }
+})
