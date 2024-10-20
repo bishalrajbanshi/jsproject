7439b37bd8cb11250fe092c4eafd22f93a5bd4ac
bishlrajbanshi <bishalrajbanshi.in@gmail.com>
2024-10-20 17:33:16 +0545

    todo

---
+++ TODO/script.js
@@ -0,0 +1,34 @@
+let userInput = document.getElementById('userinput')
+let add = document.getElementById('add')
+let list = document.getElementById('list')
+
+let info = [];
+
+add.addEventListener('click',(e)=>{
+    e.preventDefault()
+
+   const inputValue = userInput.value.trim()
+
+   //push the value in array
+   info.push(inputValue)
+   renderValue(inputValue,info.length -1)
+   userInput.value = ""
+})
+
+function renderValue(item,index){
+    const li = document.createElement('li')
+    const del = document.createElement('button')
+
+    li.innerText = item //set the text for item list
+    del.innerText = "Delete"
+
+    li.appendChild(del) //appent the delete button on list item
+    list.appendChild(li) //appent the list item on list
+
+    del.addEventListener('click',()=>{
+        info.splice(index,1)
+        li.remove()
+    })
+
+
+}
